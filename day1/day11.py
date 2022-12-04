d468c83b6303b8cf979072b5aca06f43265cf4e1
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-04 21:51:47 +0200

    Reformat code to adhere to pylint requirements

--- day1/day11.py
+++ day1/day11.py
@@ -1,16 +1,26 @@
-with open("day1/input.txt") as file:
-    lines = [line.rstrip() for line in file]
+"""Advent of code 2022, Day 1"""
 
-elf = 1
-calories = 0
-max_calories = 0
-for i, value in enumerate(lines):
-    if value != "":
-        calories = calories + int(value)
-    else:
-        elf = elf + 1
-        if calories > max_calories:
-            max_calories = calories
-        calories = 0
 
-print(max_calories)
+def day11():
+    """Day 1 task, first half"""
+
+    with open("day1/input.txt", encoding="utf-8") as file:
+        lines = [line.rstrip() for line in file]
+
+    elf = 1
+    calories = 0
+    max_calories = 0
+    for dummy, value in enumerate(lines):
+        if value != "":
+            calories = calories + int(value)
+        else:
+            elf = elf + 1
+            if calories > max_calories:
+                max_calories = calories
+            calories = 0
+
+    print(max_calories)
+
+
+if __name__ == "__main__":
+    day11()
