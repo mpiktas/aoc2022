9c5ceb7256f86cd41ad9f56011654fc79f73619b
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-04 22:18:11 +0200

    Satisfy primadona bitch that is the pylint

--- day2/day21.py
+++ day2/day21.py
@@ -1,22 +1,30 @@
-with open("day2/input.txt") as file:
-    lines = [line.rstrip() for line in file]
+"""Advent of code 2022, day 2"""
+
+POINTS = {"A": 1, "B": 2, "C": 3}
+WIN = {"AA": 3, "AB": 6, "AC": 0, "BA": 0, "BB": 3, "BC": 6, "CA": 6, "CB": 0, "CC": 3}
+MATCH_TABLE = {"X": "A", "Y": "B", "Z": "C"}
 
 
-def calculate_game_points(p1, p2, cr, points, win):
-    m = cr[p2]
-    pkey = f"{p1}{m}"
-    return win[pkey] + points[m]
+def calculate_game_points(player1, player2, match_table, points, win):
+    """Calculate the elf game points for the day 2, first part"""
+    matched2 = match_table[player2]
+    pkey = f"{player1}{matched2}"
+    return win[pkey] + points[matched2]
 
 
-points = {"A": 1, "B": 2, "C": 3}
-win = {"AA": 3, "AB": 6, "AC": 0, "BA": 0, "BB": 3, "BC": 6, "CA": 6, "CB": 0, "CC": 3}
+def day21():
+    """Day 2, part 1"""
 
-cr = {"X": "A", "Y": "B", "Z": "C"}
+    with open("day2/input.txt", encoding="utf-8") as file:
+        lines = [line.rstrip() for line in file]
+
+    tot = 0
+    for dummy, value in enumerate(lines):
+        party = value.split()
+        tot = tot + calculate_game_points(party[0], party[1], MATCH_TABLE, POINTS, WIN)
+
+    print(tot)
 
 
-tot = 0
-for i, value in enumerate(lines):
-    p = value.split()
-    tot = tot + calculate_game_points(p[0], p[1], cr, points, win)
-
-print(tot)
+if __name__ == "__main__":
+    day21()
