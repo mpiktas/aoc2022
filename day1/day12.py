6150444794f8596418ebbdea19c2fa055f5ce62e
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-04 21:54:33 +0200

    Reformat according to pylint guidelines

--- day1/day12.py
+++ day1/day12.py
@@ -1,16 +1,23 @@
-with open("day1/input.txt") as file:
-    lines = [line.rstrip() for line in file]
+"""Advent of code 2022, Day 1"""
 
 
-calories = 0
-elf = []
-for i, value in enumerate(lines):
-    if value != "":
-        calories = calories + int(value)
-    else:
-        elf = elf + [calories]
-        calories = 0
+def day12():
+    """Day 1, part 2"""
+    with open("day1/input.txt", encoding="utf-8") as file:
+        lines = [line.rstrip() for line in file]
+
+    calories = 0
+    elf = []
+    for dummy, value in enumerate(lines):
+        if value != "":
+            calories = calories + int(value)
+        else:
+            elf = elf + [calories]
+            calories = 0
+
+    elf.sort()
+    print(sum(elf[-3:]))
 
 
-elf.sort()
-print(sum(elf[-3:]))
+if __name__ == "__main__":
+    day12()
