9fac32dd89b71cb812b225062ca3c71cfef2c0d3
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-04 16:40:51 +0200

    Complete task4

---
+++ task4/task4.py
@@ -0,0 +1,28 @@
+import string
+
+priorities = list(string.ascii_lowercase)+list(string.ascii_uppercase)
+
+def calculate_priority(s, priorities = priorities):
+    n = len(s)//2
+    i = list(set(s[:n]).intersection(s[-n:]))
+    if i:
+        return priorities.index(i[0])+1
+    else:
+        print(s)
+        print(i)
+        return 0
+
+
+with open("task4/input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+tot = 0
+for i, value in enumerate(lines):
+    tot = tot + calculate_priority(value)
+
+print(tot)
+
+pp = [calculate_priority(x) for x in lines]
+
+print(sum(pp))
+
