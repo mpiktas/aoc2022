97b45593f2184ca77d08691f8dd5bd66a541594f
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-04 19:40:52 +0200

    Do a renaming. All code for the day resides in the day directory

---
+++ day1/day12.py
@@ -0,0 +1,20 @@
+with open("task1/input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+
+calories = 0
+elf = []
+for i, value in enumerate(lines):
+    if value != '':
+        calories = calories + int(value)
+    else:
+        elf = elf + [calories]        
+        calories = 0
+        
+
+elf.sort()       
+print(sum(elf[-3:]))
+
+
+
+
---
+++ day4/day42.py
@@ -0,0 +1,22 @@
+with open("task6/input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+def compare1(r1, r2): 
+    return ((r1[0]<=r2[1]) & (r1[1]>=r2[0]))
+
+
+def compare2(r1, r2):
+    return (compare1(r1,r2) | compare1(r2, r1))
+
+
+def str_to_int(s) :
+    return [int(x) for x in s] 
+
+def treat_line(s): 
+    s = [str_to_int(x.split("-")) for x in s.split(",")]       
+    return compare2(s[0], s[1])
+
+cs = [treat_line(x) for x in lines]
+
+
+print(sum(cs))
