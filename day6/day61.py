0279b5f69afeca4d79e179d8cbc4ee5629ac6e28
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-16 16:11:23 +0200

    Pylint day 6

--- day6/day61.py
+++ day6/day61.py
@@ -5,9 +5,9 @@ def day61():
     """Solve the part 1 of day 6"""
     with open("day6/input.txt", encoding="utf-8") as file:
         lines = [line.rstrip() for line in file]
-    input = list(lines[0])
-    for i in range(4, len(input)):
-        roll = set(input[i - 4 : i])
+    inp = list(lines[0])
+    for i in range(4, len(inp)):
+        roll = set(inp[i - 4 : i])
         if len(roll) == 4:
             print(i)
             break
--- day6/day62.py
+++ day6/day62.py
@@ -5,9 +5,9 @@ def day62():
     """Solve the part 2 of day 6"""
     with open("day6/input.txt", encoding="utf-8") as file:
         lines = [line.rstrip() for line in file]
-    input = list(lines[0])
-    for i in range(14, len(input)):
-        roll = set(input[i - 14 : i])
+    inp = list(lines[0])
+    for i in range(14, len(inp)):
+        roll = set(inp[i - 14 : i])
         if len(roll) == 14:
             print(i)
             break
