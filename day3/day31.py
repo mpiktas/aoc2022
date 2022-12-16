d4cb251399a88a72f34124410db524527236fcaf
Vaidotas Zemlys <zemlys@gmail.com>
2022-12-16 11:35:06 +0200

    Fix day31 according to pylint

--- day3/day31.py
+++ day3/day31.py
@@ -1,28 +1,35 @@
+"""Advent of Code, day 3 """
 import string
 
-priorities = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+PRIORITIES = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 
 
-def calculate_priority(s, priorities=priorities):
+def calculate_priority(s):
     n = len(s) // 2
     i = list(set(s[:n]).intersection(s[-n:]))
     if i:
-        return priorities.index(i[0]) + 1
+        return PRIORITIES.index(i[0]) + 1
     else:
         print(s)
         print(i)
         return 0
 
 
-with open("day3/input.txt") as file:
-    lines = [line.rstrip() for line in file]
+def day31():
 
-tot = 0
-for i, value in enumerate(lines):
-    tot = tot + calculate_priority(value)
+    with open("day3/input.txt") as file:
+        lines = [line.rstrip() for line in file]
 
-print(tot)
+    tot = 0
+    for i, value in enumerate(lines):
+        tot = tot + calculate_priority(value)
 
-pp = [calculate_priority(x) for x in lines]
+    print(tot)
 
-print(sum(pp))
+    pp = [calculate_priority(x) for x in lines]
+
+    print(sum(pp))
+
+
+if __name__ == "__main__":
+    day31()
